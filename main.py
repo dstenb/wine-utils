9692847c35d525fc109cbc6ed54ea0e88255fba0
dstenb <dstenb@gmail.com>
2011-07-20 12:23:34 +0200

    pep8 compliant code

--- main.py
+++ main.py
@@ -9,119 +9,127 @@ import getopt
 
 from printer import *
 
-''' Class for menu information about either a WINE program or directory '''
-class MenuEntry:
-	def __init__(self):
-		self.name = ''
-		self.cmd = ''
-		self.entries = []
 
-	''' Returns TRUE if is leaf '''
-	def is_leaf(self):
-		return not self.entries
+class MenuEntry:
+
+    ''' Class for menu information about either a WINE program or directory '''
+
+    def __init__(self):
+        self.name = ''
+        self.cmd = ''
+        self.entries = []
+
+    ''' Returns TRUE if is leaf '''
+    def is_leaf(self):
+        return not self.entries
+
 
 def read_desktop_file(entry, path):
-	''' Reads application info
+    ''' Reads application info
 
-		Arguments:
-		root = MenuEntry to put information in
-		path = Absolute path to file
-	'''
-	
-	with open(path) as f:
-		for line in f:
-			s = line.split('=', 1)
+        Arguments:
+        root = MenuEntry to put information in
+        path = Absolute path to file
+    '''
 
-			if len(s) == 2:	
-				if s[0] == "Name":
-					entry.name = s[1].rstrip('\n')
-				elif s[0] == "Exec":
-					entry.cmd = s[1].rstrip('\n')
+    with open(path) as f:
+        for line in f:
+            s = line.split('=', 1)
+
+            if len(s) == 2:
+                if s[0] == "Name":
+                    entry.name = s[1].rstrip('\n')
+                elif s[0] == "Exec":
+                    entry.cmd = s[1].rstrip('\n')
+
 
 def create_menu(root, path):
-	''' Recursively reads a given directory for .desktop files
+    ''' Recursively reads a given directory for .desktop files
 
-		Arguments:
-		root = MenuEntry that represents the root in the tree
-		path = Absolute path to directory
-	'''
+        Arguments:
+        root = MenuEntry that represents the root in the tree
+        path = Absolute path to directory
+    '''
 
-	# TODO handle exceptions
+    # TODO handle exceptions
 
-	files = os.listdir(path)
+    files = os.listdir(path)
 
-	for f in files:
-		buf = os.path.join(path, f)
-		st = os.lstat(buf)
-		
-		ent = MenuEntry()
-		root.entries.append(ent)
-		
-		if stat.S_ISDIR(st.st_mode):
-			ent.name = f
-			create_menu(ent, buf)
-		elif buf.endswith('.desktop'):
-			read_desktop_file(ent, buf)
+    for f in files:
+        buf = os.path.join(path, f)
+        st = os.lstat(buf)
+
+        ent = MenuEntry()
+        root.entries.append(ent)
+
+        if stat.S_ISDIR(st.st_mode):
+            ent.name = f
+            create_menu(ent, buf)
+        elif buf.endswith('.desktop'):
+            read_desktop_file(ent, buf)
+
 
 def usage(cmd):
-	''' Print program usage information '''
-	
-	print('Usage:', cmd, '[OPTIONS]...')
-	print()
-	print('Mandatory arguments to long options are mandatory for short options too.')
-	print()
-	print('-h, --help      print this message.')
-	print('-f, --format    select format (see format list below).')
-	print()
-	print('Format list')
-	print(' plain          Plain menu format')
-	print(' awesome        Awesome Window Manager')
-	print(' pekwm          PekWM Window Manager')
-	print(' pekwm-dynamic  PekWM Window Manager dynamic menu (see README)')
-	print()
+    ''' Print program usage information '''
 
-def main(argv = None):
-	if argv is None:
-		argv = sys.argv
+    print('Usage:', cmd, '[OPTIONS]...')
+    print()
+    print('Mandatory arguments to long options',
+        'are mandatory for short options too.')
+    print()
+    print('-h, --help      print this message.')
+    print('-f, --format    select format (see format list below).')
+    print()
+    print('Format list')
+    print(' plain          Plain menu format')
+    print(' awesome        Awesome Window Manager')
+    print(' pekwm          PekWM Window Manager')
+    print(' pekwm-dynamic  PekWM Window Manager dynamic menu (see README)')
+    print()
 
-	cmd = argv[0]
 
-	try:
-		opts, argv = getopt.getopt(argv[1:], 'f:h', [ 'help', 'format=' ])
-	except getopt.GetoptError as e:
-		print(e)
-		usage(cmd)
-		sys.exit()
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
 
-	printer = None
+    cmd = argv[0]
 
-	for o, a in opts:
-		if o in ('-h', '--help'):
-			usage(cmd)
-			sys.exit()
-		elif o in ('-f', '--format'):
-			if a == 'awesome':
-				printer = AwesomePrinter()
-			elif a == 'pekwm':
-				printer = PekWMPrinter(False)
-			elif a == 'pekwm-dynamic':
-				printer = PekWMPrinter(True)
-			elif not (a == 'plain'):
-				print('unknown choice for', o)
-				sys.exit()
+    try:
+        opts, argv = getopt.getopt(argv[1:], 'f:h', ['help', 'format='])
+    except getopt.GetoptError as e:
+        print(e)
+        usage(cmd)
+        sys.exit()
 
-	''' Get absolute path for WINE applications directory '''
-	path = os.path.expanduser('~/.local/share/applications/wine/Programs')
+    printer = None
 
-	root = MenuEntry()
-	root.name = "Wine"
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage(cmd)
+            sys.exit()
+        elif o in ('-f', '--format'):
+            if a == 'awesome':
+                printer = AwesomePrinter()
+            elif a == 'pekwm':
+                printer = PekWMPrinter(False)
+            elif a == 'pekwm-dynamic':
+                printer = PekWMPrinter(True)
+            elif not (a == 'plain'):
+                print('unknown choice for', o)
+                sys.exit()
 
-	create_menu(root, path)
+    ''' Get absolute path for WINE applications directory '''
+    path = os.path.expanduser('~/.local/share/applications/wine/Programs')
 
-	if not printer:
-		printer = Printer()
+    root = MenuEntry()
+    root.name = "Wine"
 
-	printer.print_menu(root)
+    create_menu(root, path)
+
+    if not printer:
+        printer = Printer()
+
+    printer.print_menu(root)
 
 if __name__ == "__main__":
-	main()
+    main()
--- printer.py
+++ printer.py
@@ -1,85 +1,90 @@
 import hashlib
 
-''' Base class for menu printers '''
+
 class Printer:
-	def print_menu(self, entry, lvl = 0):
-		if not entry.is_leaf():
-			print("%s%s {" % (" " * lvl, entry.name ))
-			for e in entry.entries:
-				self.print_menu(e, lvl + 1)
-			print("%s}" % " " * lvl)
-		else:
-			print("%s%s %s" % (" " * lvl, entry.name, entry.cmd))
+    ''' Base class for menu printers '''
+    def print_menu(self, entry, lvl=0):
+        if not entry.is_leaf():
+            print("%s%s {" % (" " * lvl, entry.name))
+            for e in entry.entries:
+                self.print_menu(e, lvl + 1)
+            print("%s}" % " " * lvl)
+        else:
+            print("%s%s %s" % (" " * lvl, entry.name, entry.cmd))
 
-''' Printer class for Awesome Window Manager '''
+
 class AwesomePrinter(Printer):
-	''' Print menu '''
-	def print_menu(self, entry, lvl = 0):
-		if not entry.is_leaf():
-			for e in entry.entries:
-				self.print_menu(e, lvl + 1)
+    ''' Printer class for Awesome Window Manager '''
 
-			print(self.create_ref(entry.name), '=', '{')
-			
-			stop = len(entry.entries) - 1
-			i = 0
+    ''' Print menu '''
+    def print_menu(self, entry, lvl=0):
+        if not entry.is_leaf():
+            for e in entry.entries:
+                self.print_menu(e, lvl + 1)
 
-			while i <= stop:
-				e = entry.entries[i]
+            print(self.create_ref(entry.name), '=', '{')
 
-				self.print_item(e, i == stop)
-				i += 1
-			print("}")
-			print()
+            stop = len(entry.entries) - 1
+            i = 0
 
-	''' Print menu string '''
-	def print_item(self, entry, last):
-		if entry.is_leaf():
-			self.print_leaf(entry, last)
-		else:
-			self.print_dir(entry, last)
+            while i <= stop:
+                e = entry.entries[i]
 
-	''' Print program menu string '''
-	def print_leaf(self, entry, last):
-		''' Escape " and ' '''
-		name = entry.name.replace('\'', '\\\'').replace('\"', '\\\"')
-		cmd = entry.cmd.replace('\'', '\\\'').replace('\"', '\\\"')
+                self.print_item(e, i == stop)
+                i += 1
+            print("}")
+            print()
 
-		e = '" }'
-		if last == False:
-			e += ','
-		print('   {"', name, '", "', cmd, e, sep='')
+    ''' Print menu string '''
+    def print_item(self, entry, last):
+        if entry.is_leaf():
+            self.print_leaf(entry, last)
+        else:
+            self.print_dir(entry, last)
 
-	''' Print directory menu string '''
-	def print_dir(self, entry, last):
-		name = entry.name.replace('\'', '\\\'').replace('\"', '\\\"')
-		
-		e = '}'
-		if last == False:
-			e += ','
-		print('   {"', name, '", ', self.create_ref(name), e, sep='')
+    ''' Print program menu string '''
+    def print_leaf(self, entry, last):
+        ''' Escape " and ' '''
+        name = entry.name.replace('\'', '\\\'').replace('\"', '\\\"')
+        cmd = entry.cmd.replace('\'', '\\\'').replace('\"', '\\\"')
 
-	''' Create a MD5 checksum menu name '''
-	def create_ref(self, name):
-		return "menu_" +  hashlib.md5(name.encode('UTF-8')).hexdigest()
+        e = '" }'
+        if last == False:
+            e += ','
+        print('   {"', name, '", "', cmd, e, sep='')
 
-''' Printer class for PekWM Window Manager '''
+    ''' Print directory menu string '''
+    def print_dir(self, entry, last):
+        name = entry.name.replace('\'', '\\\'').replace('\"', '\\\"')
+
+        e = '}'
+        if last == False:
+            e += ','
+        print('   {"', name, '", ', self.create_ref(name), e, sep='')
+
+    ''' Create a MD5 checksum menu name '''
+    def create_ref(self, name):
+        return "menu_" + hashlib.md5(name.encode('UTF-8')).hexdigest()
+
+
 class PekWMPrinter:
+    ''' Printer class for PekWM Window Manager '''
 
-	def __init__(self, dynamic):
-		self.dynamic = dynamic
+    def __init__(self, dynamic):
+        self.dynamic = dynamic
 
-	def print_menu(self, entry, lvl = 0):
-		name = entry.name.replace('\'', '\\\'').replace('\"', '\\\"')
+    def print_menu(self, entry, lvl=0):
+        name = entry.name.replace('\'', '\\\'').replace('\"', '\\\"')
 
-		if not entry.is_leaf():
-			if lvl == 0 and self.dynamic:
-				print("%sDynamic = \"%s\" {" % ("\t"*lvl, name))
-			else:
-				print("%sSubmenu = \"%s\" {" % ("\t"*lvl, name))
-			for e in entry.entries:
-				self.print_menu(e, lvl + 1)
-			print("\t" * lvl,"}", sep="")
-		else:
-			cmd = entry.cmd.replace('\'', '\\\'').replace('\"', '\\\"')
-			print("%sEntry = \"%s\" { Actions = \"Exec %s &\" }" % ("\t"*lvl, name, cmd))
+        if not entry.is_leaf():
+            if lvl == 0 and self.dynamic:
+                print("%sDynamic = \"%s\" {" % ("\t" * lvl, name))
+            else:
+                print("%sSubmenu = \"%s\" {" % ("\t" * lvl, name))
+            for e in entry.entries:
+                self.print_menu(e, lvl + 1)
+            print("\t" * lvl, "}", sep="")
+        else:
+            cmd = entry.cmd.replace('\'', '\\\'').replace('\"', '\\\"')
+            print("%sEntry = \"%s\" { Actions = \"Exec %s &\" }" % (
+                "\t" * lvl, name, cmd))
